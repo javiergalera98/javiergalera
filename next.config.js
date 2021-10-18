5c1c5eb7bd05dddb60b8afddf6d913de7736c28a
Javier Galera Robles <jgalerarobles@icloud.com>
2021-10-18 12:46:37 +0200

    Installed Sass and NextTranslate

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate')
+const path = require('path')
+
+module.exports = nextTranslate({
+    env: {
+        PUBLIC_URL: '/static/',
+    },
+    webpack: (config, { isServer, webpack }) => {
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+    }
+})
