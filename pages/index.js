7422bab27921f87196fc7d8698cb91c9e40ff56d
Javier Galera Robles <jgalerarobles@icloud.com>
2021-10-19 14:58:36 +0200

    Changes in the index.js - Added SEO to index.js - UI changes to the welcome banner to fit a new image, with new color and texts.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import useTranslation from "next-translate/useTranslation";
+import { NextSeo } from 'next-seo';
 
 import Header from "../components/global/header";
 import Footer from "../components/global/footer";
@@ -9,10 +11,21 @@ export default function Home() {
   const { t } = useTranslation("global");
 
   return (
-    <div>
+    <>
+    <NextSeo
+      title={t("seoTitle")}
+      description={t("seoDescription")}
+      canonical="https://www.javiergalera.com/"
+      openGraph={{
+        url: 'https://www.javiergalera.com/',
+        title: 'Javier Galera',
+        description: 'Open Graph Description',
+      }}
+    />
       <Header />
       <div className="content">
         <div className="welcome-banner">
+          <Image src="/images/avatar.webp" alt="Picture of the author" width={150} height={150}/>
           <h2>{t("welcomeTitle")}</h2>
           <h1>{t("welcomeSubtitle")}</h1>
           <p>{t("welcomeDescription1")}<br/>{t("welcomeDescription2")} <a className="font-bold" href="https://www.byspace.es">Space.</a></p>
@@ -20,6 +33,6 @@ export default function Home() {
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
